1eafcb751114703b0693769c3a24221eba664bdb
masa3 <masa@hashicorp.com>
2020-10-06 16:34:01 +0900

    removed 1b from az

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ module "masa_vpc" {
 
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-northeast-1a", "ap-northeast-1b"]
+  azs             = ["ap-northeast-1a", "ap-northeast-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
