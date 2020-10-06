da4ef34b2bb990a7d7599d6a4d712b8c602e909f
masa3 <masa@hashicorp.com>
2020-10-06 22:19:56 +0900

    removed nat gateway

--- vpc.tf
+++ vpc.tf
@@ -23,13 +23,13 @@ module "vpc_japan" {
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-northeast-1a"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_ipv6 = true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = false
+  single_nat_gateway = false
 
   public_subnet_tags = {
     Name = "overridden-name-public"
@@ -78,13 +78,13 @@ module "vpc_sydney" {
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-southeast-2a"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_ipv6 = true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = false
+  single_nat_gateway = false
 
   public_subnet_tags = {
     Name = "overridden-name-public"
