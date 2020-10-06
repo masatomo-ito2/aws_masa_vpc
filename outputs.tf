0b0b161938545b367689611b6b7a2b6a3fbcd045
masa3 <masa@hashicorp.com>
2020-10-06 16:56:41 +0900

    added outputs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "security_group_ssh" {
+	value = module.ssh_sg.this_security_group_id
+}
+
+output "vpc_id" {
+	value = module.masa_vpc.default_vpc_id
+}
+
+output "public_subnets" {
+	value = module.masa_vpc.public_subnets
+}
