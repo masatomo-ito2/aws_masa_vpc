516ba5146c10901505b284eb51f68742a27376ad
masa3 <masa@hashicorp.com>
2021-02-03 10:46:16 +0900

    disable ipv6 in sydney

--- vpc.tf
+++ vpc.tf
@@ -68,7 +68,7 @@ module "vpc_sydney" {
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
-  enable_ipv6 = true
+	# enable_ipv6 = true
 
   enable_dns_hostnames = true
   enable_dns_support   = true
