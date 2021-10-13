22cbe48579160104e16245dc44af12298b7c1640
Masa Ito <masa@hashicorp.com>
2021-10-13 09:53:56 +0900

    output

--- outputs.tf
+++ outputs.tf
@@ -18,10 +18,21 @@ output "public_subnets_sydney" {
 
 # HCP
 
+locals {
+  v = data.hcp_vault_cluster.vault_cluster
+}
+
 output "vault_public_url" {
-  value = data.hcp_vault_cluster.vault_cluster.vault_public_endpoint_url
+  value = local.v.vault_public_endpoint_url
 }
 
 output "vault_private_url" {
-  value = data.hcp_vault_cluster.vault_cluster.vault_private_endpoint_url
+  value = local.v.vault_private_endpoint_url
+}
+
+output "vault_info" {
+
+  value = <<EOF
+cloud_provider = ${local.v.cloud_provider}
+EOF
 }
